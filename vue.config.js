2b1cfacbc5c59020b61b87690741f4363638aa54
VincentJonathan <vinjongm@gmail.com>
2020-05-21 17:50:24 +0700

    Fix typo

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   chainWebpack: config => {
     config.plugin("html").tap(args => {
-      args[0].title = "Vincent Jonatahn";
+      args[0].title = "Vincent Jonathan";
       return args;
     });
   }
