70a983fe6c16f36276225277466e631a0c3d1d26
VincentJonathan <vinjongm@gmail.com>
2020-05-29 20:39:18 +0700

    Change hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,18 @@
 <template>
   <section id="hero">
-    <v-img class="bg-main" src="@/assets/img/bg.jpg">
-      <v-row
-        class="fill-height text-right pa-md-5 pa-0 mx-0 align-center justify-center"
-      >
-        <div class="text-center bg-dark pa-8">
-          <div class="text-uppercase display-1">Vincent Jonathan</div>
-          <h1 class="primary--text text-uppercase display-3">
+    <v-img class="bg-main">
+      <v-row class="fill-height mx-0 align-center justify-end">
+        <div class="text-center py-8 mr-4 pa-md-12 mr-md-12">
+          <div
+            class="text-uppercase text-right"
+            :class="$vuetify.breakpoint.lgAndUp ? 'display-3' : 'display-2'"
+          >
+            Vincent Jonathan
+          </div>
+          <h1
+            class="primary--text text-uppercase text-right"
+            :class="$vuetify.breakpoint.lgAndUp ? 'display-4' : 'display-2'"
+          >
             FULL STACK DEVELOPER
           </h1>
         </div>
